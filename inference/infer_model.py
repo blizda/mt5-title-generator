a1ef07f38848f02c87b625d4ab2c44f48ae876b4
Danil <blizda@outlook.com>
2021-01-20 22:23:20 +0300

    add model inference

--- inference/infer_model.py
+++ inference/infer_model.py
@@ -1,6 +1,15 @@
 import falcon
 import json
 from speed_up_t5_onnx import OnnxT5
+import wandb
+import glob
+import os
+from transformers import T5Tokenizer
+
+def get_model_from_w_b_without_run(artifact_name='T5_model_tasks:latest'):
+    api = wandb.Api()
+    artifact = api.artifact('blizd/mt-t5-ria-news/'+artifact_name, type='model')
+    return artifact.download()
 
 class ProcessMassge(object):
     def on_post(self, req, resp):
@@ -9,6 +18,10 @@ class ProcessMassge(object):
         resp.status = falcon.HTTP_200
         return resp
 
+path = get_model_from_w_b_without_run()
+model_path = glob.glob(os.path.join(path, '*.pt'))[0]
+tokenizer = T5Tokenizer.from_pretrained('google/mt5-small')
+onnx_model = OnnxT5(model_name_or_path="google/mt5-small", onnx_path="onnx_models", model_st_d=model_path)
 app = falcon.API()
 message_processor = ProcessMassge()
 app.add_route('/model', message_processor)
